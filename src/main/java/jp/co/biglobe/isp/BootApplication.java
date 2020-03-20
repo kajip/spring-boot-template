51f7d494fb4e72ee577de1aec593786cb37ee48d
Tomomi Kajita <t-kajita@biglobe.co.jp>
2020-03-21 00:41:37 +0900

    DockerイメージのタグフォーマットをOCIフォーマットに変更
    Signed-off-by: Tomomi Kajita <t-kajita@biglobe.co.jp>

--- src/main/java/jp/co/biglobe/isp/BootApplication.java
+++ src/main/java/jp/co/biglobe/isp/BootApplication.java
@@ -9,15 +9,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-    ServiceProperties.class
-})
+@EnableConfigurationProperties({ ServiceProperties.class })
 @EnableAsync
 public class BootApplication {
 
     public static void main(String args[]) {
-        ConfigurableApplicationContext context =
-            SpringApplication.run(BootApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(BootApplication.class, args);
 
         // シグナルハンドラの登録
         GracefulShutdownHandler.regist(context);
